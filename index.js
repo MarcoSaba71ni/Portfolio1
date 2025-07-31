75289b3d44fcc9ea1e494e98b99416758a5d9b85
Marco Sabatini <marsab04188@stud.noroff.no>
2025-07-31 23:52:42 +0200

    Revamp about and projects sections, update styles
    Enhanced the about section with more detailed content and interactive show/hide buttons. Refactored the projects section for clarity and added a gallery to the athlete section. Updated styling for improved layout, responsiveness, and visual consistency. Also updated Marco Sabatini's profile picture.

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+// About Section
+
+const aboutBtn = document.getElementById("about-btn");
+const moreAbout = document.getElementById("more-about");
+const aboutLess = document.getElementById("about-less");
+const lessAbout = document.getElementById("less-about");
+const aboutTxt = document.getElementById("about-txt");
+
+aboutBtn.addEventListener("click", () => {
+  moreAbout.style.display = "block";
+  moreAbout.scrollIntoView ({behavior: "smooth"});
+  aboutBtn.style.display = "none";
+  lessAbout.style.display = "block";
+})
+
+lessAbout.addEventListener("click", () => {
+  moreAbout.style.display = "none";
+  lessAbout.style.display = "none";
+  aboutBtn.style.display = "block";
+  moreAbout.scrollIntoView ({behavior: "smooth"});
+})
+
+// Other Side Section
+
 const moreBtn = document.getElementById("more-btn");
 const fullContent = document.getElementById("full-content");
 const heroContent = document.getElementById("hero-content");
+const overlayTxt = document.getElementById("overlay-txt");
+const lessBtn = document.getElementById("less-btn");
 
 moreBtn.addEventListener("click", () => {
-  heroContent.style.display = "none";
+  overlayTxt.style.display = "none";
+  heroContent.style.display = "block";
   fullContent.style.display = "block";
   fullContent.scrollIntoView({ behavior: "smooth" });
 });
+
+lessBtn.addEventListener("click", () => {
+  fullContent.style.display = "none";
+  overlayTxt.style.display = "block";
+  heroContent.scrollIntoView({ behavior: "smooth" });
+})
