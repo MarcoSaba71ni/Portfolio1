64852fb3e05d160c1728909a57c37274f507c261
Marco Sabatini <marsab04188@stud.noroff.no>
2025-08-07 15:19:59 +0200

    Improve section toggle UX and update styles
    Enhanced the About and Other Side section toggling with smoother transitions and delayed scrolls for better user experience. Updated CSS to ensure proper centering and display of elements involved in section toggling.

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 // About Section
-
-const aboutBtn = document.getElementById("about-btn");
-const moreAbout = document.getElementById("more-about");
-const aboutLess = document.getElementById("about-less");
-const lessAbout = document.getElementById("less-about");
-const aboutTxt = document.getElementById("about-txt");
-const divMore = document.getElementById("div-more");
+const aboutBtn = document.getElementById("about-btn"); 
+const divMore = document.getElementById("div-more"); 
+const aboutLess = document.getElementById("about-less"); 
+const lessAbout = document.getElementById("less-about"); 
+const aboutTxt = document.getElementById("about-txt");  
+const moreAbout = document.getElementById("more-about");  
 
 aboutBtn.addEventListener("click", () => {
-  moreAbout.style.display = "block";
-  moreAbout.scrollIntoView ({behavior: "smooth"});
-  aboutBtn.style.display = "none";
-  lessAbout.style.display = "block";
-})
+  moreAbout.style.display = "block"; 
+  moreAbout.scrollIntoView({ behavior: "smooth" }); 
+  divMore.style.display = "none";
+  lessAbout.style.display = "block"; 
+});
 
 lessAbout.addEventListener("click", () => {
-  moreAbout.style.display = "none";
-  lessAbout.style.display = "none";
-  aboutBtn.style.display = "block";
-})
+  aboutTxt.scrollIntoView({ behavior: "smooth" });
 
-// Other Side Section
+  setTimeout(() => {
+    moreAbout.style.display = "none";
+    lessAbout.style.display = "none";
+    divMore.style.display = "flex";
 
+    setTimeout(() => {
+      aboutTxt.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }, 500);
+});
+
+// Other side Section
 const moreBtn = document.getElementById("more-btn");
 const fullContent = document.getElementById("full-content");
 const heroContent = document.getElementById("hero-content");
@@ -30,13 +36,20 @@ const lessBtn = document.getElementById("less-btn");
 
 moreBtn.addEventListener("click", () => {
   overlayTxt.style.display = "none";
-  heroContent.style.display = "block";
   fullContent.style.display = "block";
   fullContent.scrollIntoView({ behavior: "smooth" });
 });
 
 lessBtn.addEventListener("click", () => {
-  fullContent.style.display = "none";
-  overlayTxt.style.display = "block";
   heroContent.scrollIntoView({ behavior: "smooth" });
-})
+
+  setTimeout(() => {
+    fullContent.style.display = "none";
+    overlayTxt.style.display = "block";
+
+    setTimeout(() => {
+      heroContent.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+
+  }, 500);
+});
